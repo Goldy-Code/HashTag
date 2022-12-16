03b753180625f469d837992aa9df03df7e7d4025
Goldy-Code <113247980+Goldy-Code@users.noreply.github.com>
2022-12-16 23:15:12 +0500

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,25 @@
+//// VELIK
+let hashTag = () => {
+	let str = prompt('');
+	str = str.trim();
+	str = str.split(' ');
+	let newStr = '#';
+	str
+	for (const i of str) {
+		newStr += i[0].toUpperCase() + i.slice(1).toLowerCase()
+	}
+	return console.log(newStr);
+}
+hashTag();
+
+//// NE VELIK
+let tagHash = () => {
+	let str = prompt('');
+	let newStr = str.trim()
+		.split(' ')
+		.map((words, index) => index == -1 ? words : words[0].toUpperCase() + words.slice(1).toLowerCase())
+		.join('');
+	let tag = '#' + newStr;
+	return console.log(tag);
+}
+// tagHash();
