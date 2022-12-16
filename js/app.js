256ad0b6f946132b5c9d023178a2bde60d5efe25
Goldy-Code <113247980+Goldy-Code@users.noreply.github.com>
2022-12-17 00:17:40 +0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
 //// VELIK
 let hashTag = () => {
 	let str = prompt('');
-	str = str.trim();
+	// str = str.trim();
 	str = str.split(' ');
 	let newStr = '#';
-	str
 	for (const i of str) {
-		newStr += i[0].toUpperCase() + i.slice(1).toLowerCase()
+		if (!(i == '')) {
+			newStr += i[0].toUpperCase() + i.slice(1).toLowerCase()
+		}
 	}
 	return console.log(newStr);
 }
@@ -17,7 +18,7 @@ let tagHash = () => {
 	let str = prompt('');
 	let newStr = str.trim()
 		.split(' ')
-		.map((words, index) => index == -1 ? words : words[0].toUpperCase() + words.slice(1).toLowerCase())
+		.map((words) => words == '' ? words : words[0].toUpperCase() + words.slice(1).toLowerCase())
 		.join('');
 	let tag = '#' + newStr;
 	return console.log(tag);
